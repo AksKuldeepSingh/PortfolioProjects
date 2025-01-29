ea88d85f2439ec3ca6ee9faa2841351de95c254a
Aks Kuldeep Singh <98533367+AksKuldeepSingh@users.noreply.github.com>
2025-01-29 17:42:35 +0530

    Update ssms project v2.sql
    Ctrl v

--- ssms project v2.sql
+++ ssms project v2.sql
@@ -1,113 +1,119 @@
+-- The dataset contains global Covid deaths and vaccinations data
+
 SELECT*
 FROM PortfolioProject..CovidDeaths
 ORDER BY 3,4
 ;
 
---SELECT*
---FROM PortfolioProject..CovidVaccinations
---ORDER BY 3,4
---;
+SELECT*
+FROM PortfolioProject..CovidVaccinations
+ORDER BY 3,4
+;
 
--- Changing date format
+-- Changing date format to YYYY-MM-DD
 
-EXEC sp_rename 'CovidDeaths.date','test_date','COLUMN';
+SELECT CAST(date AS DATE) AS DateOnly
+FROM PortfolioProject.dbo.CovidDeaths;
 
-SELECT COLUMN_NAME, DATA_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'CovidDeaths' AND COLUMN_NAME = 'test_date';
+ALTER TABLE PortfolioProject.dbo.CovidDeaths
+ADD DateOnly DATE;
 
-SELECT CONVERT(VARCHAR(10), CAST(test_date AS DATE), 111) AS formatted_date
-FROM CovidDeaths;
+UPDATE PortfolioProject.dbo.CovidDeaths
+SET DateOnly = CAST(date AS DATE);
 
-UPDATE CovidDeaths
-SET test_date = CONVERT(VARCHAR(10), CAST(test_date AS DATE), 111);
+SELECT date, DateOnly
+FROM PortfolioProject.dbo.CovidDeaths;
 
 -- Using formatted date
 
-SELECT Location, test_date, total_cases, new_cases, total_deaths, population
+SELECT Location, DateOnly, total_cases, new_cases, total_deaths, population
 FROM PortfolioProject..CovidDeaths
-ORDER BY 1,2
+ORDER BY 1,2 asc;
 
 -- Looking at Total Cases vs Total Deaths
 
+EXEC sp_columns 'CovidDeaths';
+
 SELECT 
     CAST(CASE 
-            WHEN total_deaths = '' THEN 0 
+            WHEN total_deaths is null THEN 0 
             ELSE total_deaths 
          END AS FLOAT) / 
     CAST(CASE 
-            WHEN total_cases = '' THEN 1 -- Avoid division by zero if cases are blank
+            WHEN total_cases is null THEN 1 -- Avoid division by zero if cases are NULL
             ELSE total_cases 
          END AS FLOAT) AS case_fatality_rate
 FROM 
     CovidDeaths
+	;
 
---Shows the likelihood of dying if infected in your country
+-- Shows the likelihood of dying if infected (in your country of choice)
 
-SELECT Location, test_date, total_cases, total_deaths, (SELECT 
+SELECT Location, DateOnly, total_cases, total_deaths, (SELECT 
     CAST(CASE 
-            WHEN total_deaths = '' THEN 0 
+            WHEN total_deaths is null THEN 0 
             ELSE total_deaths 
          END AS FLOAT) / 
     CAST(CASE 
-            WHEN total_cases = '' THEN 1 -- Avoids division by zero if cases are blank
+            WHEN total_cases is null THEN 1 -- Avoids division by zero if cases are NULL
             ELSE total_cases 
          END AS FLOAT))*100 AS case_fatality_rate
 FROM PortfolioProject..CovidDeaths
 WHERE location like '%states%'
-ORDER BY 1,2
+ORDER BY 1,2;
 
--- Looking at the Total Cases vs Population
+--SELECT COUNT(*) AS null_count
+--FROM CovidDeaths
+--WHERE total_cases IS NULL;
 
---Shows percentage infected in your country
 
-SELECT Location, test_date, total_cases, population, (SELECT 
-    CAST(CASE 
-            WHEN total_cases = '' THEN 0 
-            ELSE total_cases 
-         END AS FLOAT) / 
-    CAST(CASE 
-            WHEN population = '' THEN 1 -- Avoids division by zero if cases are blank
-            ELSE population 
-         END AS FLOAT))*100 AS percentage_infected
+--SELECT *, 
+    --CAST(CASE 
+            --WHEN total_deaths IS NULL THEN 0 
+            --ELSE total_deaths 
+         --END AS FLOAT) / 
+    --CAST(CASE 
+           --WHEN total_cases IS NULL THEN 1 
+            --ELSE total_cases 
+         --END AS FLOAT) AS case_fatality_rate
+--FROM CovidDeaths
+--WHERE total_cases IS NULL;
+
+
+-- Looking at Total Cases vs Population
+
+-- Shows percentage infected (in your country of choice)
+
+
+SELECT Location, 
+       DateOnly, 
+       total_cases, 
+       population, 
+       (SELECT 
+           CAST(CASE 
+                   WHEN total_cases IS NULL THEN 0 
+                   ELSE total_cases 
+                END AS FLOAT) / 
+           NULLIF(CAST(population AS FLOAT), 0) -- Returns NULL if population is NULL
+        ) * 100 AS percentage_infected
 FROM PortfolioProject..CovidDeaths
 WHERE location like '%states%'
-ORDER BY 1,2
+ORDER BY 1, 2;
 
---Shows percentage infected in all countries
 
-SELECT 
-    Location, 
-    test_date, 
-    total_cases, 
-    population, 
-    (CAST(CASE 
-            WHEN total_cases = '' THEN 0 
-            ELSE CAST(total_cases AS BIGINT) -- Explicitly cast to BIGINT first
-         END AS FLOAT) / 
-    CAST(CASE 
-            WHEN population = '' THEN 1 -- Avoid division by zero if population is blank
-            ELSE CAST(population AS BIGINT)
-         END AS FLOAT)) * 100 AS percentage_infected
-FROM 
-    PortfolioProject..CovidDeaths
-ORDER BY 
-    1, 2;
+-- Looking at countries with the highest infection rate (i.e. percentage_infected)
 
--- Looking at countries with the highest Infection rate (i.e. percentage_infected)
+EXEC sp_columns 'CovidDeaths';
 
 SELECT 
     Location,  
     MAX(CAST(total_cases AS BIGINT)) AS highest_infection_count, 
     population, 
     MAX((CAST(CASE 
-            WHEN total_cases = '' THEN 0 
-            ELSE CAST(total_cases AS BIGINT) -- Explicitly cast to BIGINT first
-         END AS FLOAT) / 
-    CAST(CASE 
-            WHEN population = '' THEN 1 -- Avoid division by zero if population is blank
-            ELSE CAST(population AS BIGINT)
-         END AS FLOAT))) * 100 AS percentage_infected
+                   WHEN total_cases IS NULL THEN 0 
+                   ELSE total_cases 
+                END AS FLOAT) / 
+           NULLIF(CAST(population AS FLOAT), 0))) * 100 AS percentage_infected
 FROM 
     PortfolioProject..CovidDeaths
 	WHERE Location !='International'
@@ -115,47 +121,68 @@ FROM
 ORDER BY 
     percentage_infected DESC;
 
--- Showing countries with the highest death count per population
 
---Breakdown by country
+-- Showing countries with the highest total death count
+
+-- Breakdown by country
 
 SELECT 
     Location,  
-    MAX(CAST(total_deaths as INT)) AS total_death_count
+    max(COALESCE(CAST(total_deaths AS INT), 0)) AS total_death_count
 FROM 
     PortfolioProject..CovidDeaths
-	WHERE continent !='' -- Where continent is not blank
+	WHERE continent is not null
 	GROUP BY Location
 ORDER BY 
     total_death_count DESC;
+
 
 -- Breakdown by continent
 
 SELECT 
     location,  
-    MAX(CAST(total_deaths as INT)) AS total_death_count
+    max(COALESCE(CAST(total_deaths AS INT), 0)) AS total_death_count
 FROM 
     PortfolioProject..CovidDeaths
-	WHERE continent ='' -- Where continent is blank
+	WHERE continent is null
 	GROUP BY location
 ORDER BY 
     total_death_count DESC;
 
+-- Since the data includes overlapping regions like EU, Europe and International...
+-- The World total deaths and the sum of all other regions does not add up but this approach has been verified
+
+
 --Showing the continents with the highest death count
+
+SELECT DISTINCT continent FROM PortfolioProject..CovidDeaths;
+
 
 SELECT 
     continent,  
-    MAX(CAST(total_deaths as INT)) AS total_death_count
+    MAX(COALESCE(CAST(total_deaths AS INT), 0)) AS total_death_count
 FROM 
     PortfolioProject..CovidDeaths
-	WHERE continent !='' -- Where continent is not blank
+	WHERE continent is not null
 	GROUP BY continent
 ORDER BY 
     total_death_count DESC;
 
---Global Numbers
 
-SELECT test_date, SUM(CAST(new_cases AS INT)) as total_cases, SUM(CAST(new_deaths AS INT)) as total_deaths,
+--SELECT continent, SUM(latest_deaths) AS sum_of_countries
+--FROM (
+    --SELECT continent, location, MAX(COALESCE(CAST(total_deaths AS INT), 0)) AS latest_deaths
+    --FROM PortfolioProject..CovidDeaths
+    --WHERE continent IS NOT NULL
+    --GROUP BY continent, location
+--) country_totals
+--GROUP BY continent
+--ORDER BY sum_of_countries DESC;
+
+
+-- Global Numbers
+
+SELECT DateOnly, SUM(CAST(new_cases AS INT)) as total_cases, SUM(CAST(new_deaths AS INT)) as total_deaths,
 CASE 
         WHEN SUM(CAST(new_cases AS INT)) > 0 
         THEN (SUM(CAST(new_deaths AS INT)) * 1.0 / NULLIF(SUM(CAST(new_cases AS INT)), 0)) * 100 
@@ -163,11 +190,11 @@ CASE
 END AS case_fatality_rate
 FROM PortfolioProject..CovidDeaths
 --WHERE location like '%states%'
-WHERE continent !=''
-GROUP BY test_date
-ORDER BY 1,2
+WHERE continent is not null
+GROUP BY DateOnly
+ORDER BY 1,2;
 
---Global aggregate numbers
+-- Global aggregate numbers
 
 SELECT SUM(CAST(new_cases AS INT)) as total_cases, SUM(CAST(new_deaths AS INT)) as total_deaths,
 CASE 
@@ -177,95 +204,101 @@ CASE
 END AS case_fatality_rate
 FROM PortfolioProject..CovidDeaths
 --WHERE location like '%states%'
-WHERE continent !=''
---GROUP BY test_date
-ORDER BY 1,2
+WHERE continent is not null
+--GROUP BY DateOnly
+ORDER BY 1,2;
 
---Covid Vaccinations
---Formatting date...again
 
-EXEC sp_rename 'CovidVaccinations.date','test_date','COLUMN';
+-- Covid Vaccinations
 
-SELECT COLUMN_NAME, DATA_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'CovidVaccinations' AND COLUMN_NAME = 'test_date'; 
+-- Formatting date to YYYY-MM-DD
 
-SELECT CONVERT(VARCHAR(10), CAST(test_date AS DATE), 111) AS formatted_date
-FROM CovidVaccinations;
+SELECT CAST(date AS DATE) AS DateOnly
+FROM PortfolioProject.dbo.CovidVaccinations;
 
-UPDATE CovidVaccinations
-SET test_date = CONVERT(VARCHAR(10), CAST(test_date AS DATE), 111)
+ALTER TABLE PortfolioProject.dbo.CovidVaccinations
+ADD DateOnly DATE;
 
---Looking at total population vs vaccinations
+UPDATE PortfolioProject.dbo.CovidVaccinations
+SET DateOnly = CAST(date AS DATE);
 
-Select dea.continent,dea.location,dea.test_date,dea.population,vac.new_vaccinations
-,SUM(convert(int,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.test_date) as rolling_ppl_vaccinated
---,(rolling_ppl_vaccinated/population)*100
+SELECT date, DateOnly
+FROM PortfolioProject.dbo.CovidVaccinations;
+
+
+-- Looking at total population vs vaccinations
+
+Select dea.continent,dea.location,dea.DateOnly,dea.population,vac.new_vaccinations
+,SUM(convert(int,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.DateOnly) as rolling_ppl_vaccinated
 From CovidDeaths dea
 Join CovidVaccinations vac
 on dea.location=vac.location
-and dea.test_date=vac.test_date
-WHERE dea.continent!=''
-ORDER BY 2,3
+and dea.DateOnly=vac.DateOnly
+WHERE dea.continent is not null
+ORDER BY 2,3;
 
---use CTE
 
-With pop_vs_vac (continent, location,test_date,population,new_vaccinations, rolling_ppl_vaccinated)
+-- Using CTE
+
+With pop_vs_vac (continent, location,DateOnly,population,new_vaccinations, rolling_ppl_vaccinated)
 as
 (
-Select dea.continent,dea.location,dea.test_date,dea.population,vac.new_vaccinations
-,SUM(convert(int,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.test_date) as rolling_ppl_vaccinated
---,(rolling_ppl_vaccinated/population)*100
+Select dea.continent,dea.location,dea.DateOnly,dea.population,vac.new_vaccinations
+,SUM(convert(int,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.DateOnly) as rolling_ppl_vaccinated
 From CovidDeaths dea
 Join CovidVaccinations vac
 on dea.location=vac.location
-and dea.test_date=vac.test_date
-WHERE dea.continent!=''
---ORDER BY 2,3
+and dea.DateOnly=vac.DateOnly
+WHERE dea.continent is not null
 )
 Select *, (CAST(CASE 
-            WHEN rolling_ppl_vaccinated = '' THEN 0 
+            WHEN rolling_ppl_vaccinated is null THEN 0 
             ELSE rolling_ppl_vaccinated
          END AS FLOAT) / 
     CAST(CASE 
-            WHEN population = '' THEN 1 -- Avoids division by zero if cases are blank
+            WHEN population is null THEN 1 -- Avoids division by zero if cases are blank
             ELSE population 
          END AS FLOAT))*100
 from pop_vs_vac
-ORDER BY 2,3
+ORDER BY 2,3;
 
---Temp table
+
+-- Temp table
 
 DROP TABLE IF EXISTS #percent_population_vaccinated;
-
 Create Table #percent_population_vaccinated
 (
     continent nvarchar(255), 
     location nvarchar(255),
-    test_date datetime,
+    DateOnly datetime,
     population numeric,
     new_vaccinations numeric,
     rolling_ppl_vaccinated numeric
-);
-
--- Insert data with conversions and handling non-numeric values
+)
 Insert into #percent_population_vaccinated
-Select dea.continent,
-       dea.location,
-       dea.test_date,
-       dea.population,
-       TRY_CAST(vac.new_vaccinations AS numeric) as new_vaccinations,
-       SUM(TRY_CAST(vac.new_vaccinations AS numeric)) 
-           OVER (Partition by dea.location 
-                 Order by dea.location, dea.test_date) as rolling_ppl_vaccinated
+Select dea.continent, dea.location, dea.DateOnly, dea.population, vac.new_vaccinations
+, SUM(Convert(int, vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.DateOnly) as rolling_ppl_vaccinated
 From CovidDeaths dea
 Join CovidVaccinations vac
     on dea.location = vac.location
-    and dea.test_date = vac.test_date
---Where dea.continent != ''
-  AND TRY_CAST(vac.new_vaccinations AS numeric) IS NOT NULL -- Filter out non-numeric data
+    and dea.DateOnly = vac.DateOnly
+--Where dea.continent is not null
+--Order by 2,3;
+
+select *, (CAST(CASE 
+            WHEN rolling_ppl_vaccinated is null THEN 0 
+            ELSE rolling_ppl_vaccinated
+         END AS FLOAT) / 
+    CAST(CASE 
+            WHEN population is null THEN 1 -- Avoids division by zero if cases are blank
+            ELSE population 
+         END AS FLOAT))*100
+from #percent_population_vaccinated
+order by 2,3;
+
 
 -- Calculate percentage of population vaccinated
+
 Select *,
        (CAST(CASE 
                 WHEN rolling_ppl_vaccinated IS NULL THEN 0 
@@ -278,17 +311,17 @@ Select *,
 From #percent_population_vaccinated
 Order By 2, 3;
 
---Creating view to store data for visualizations later
+
+-- Creating a view to store data for visualizations later
+
 Create view percent_population_vaccinated as 
-Select dea.continent,dea.location,dea.test_date,dea.population,vac.new_vaccinations
-,SUM(convert(int,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.test_date) as rolling_ppl_vaccinated
---,(rolling_ppl_vaccinated/population)*100
+Select dea.continent,dea.location,dea.DateOnly,dea.population,vac.new_vaccinations
+,SUM(convert(int,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.DateOnly) as rolling_ppl_vaccinated
 From CovidDeaths dea
 Join CovidVaccinations vac
 on dea.location=vac.location
-and dea.test_date=vac.test_date
-WHERE dea.continent!=''
---ORDER BY 2,3
+and dea.DateOnly=vac.DateOnly
+WHERE dea.continent is not null
 
 select *
 from percent_population_vaccinated
